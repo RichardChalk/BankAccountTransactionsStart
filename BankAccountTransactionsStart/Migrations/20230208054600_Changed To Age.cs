b85335949e4108fa83636d417a816c9e178f9b5b
Richard Chalk <barmybrit@gmail.com>
2023-02-08 06:48:37 +0100

    2023-02-08

---
+++ BankAccountTransactionsStart/Migrations/20230208054600_Changed To Age.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkysFormsDemo.Migrations
+{
+    public partial class ChangedToAge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CarCount",
+                table: "Person",
+                newName: "Age");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Person",
+                newName: "CarCount");
+        }
+    }
+}
